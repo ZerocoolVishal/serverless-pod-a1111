1ee8c1d869aa13c9ed122fd5ad06fc4a35b954e1
Vishal <zerocoolvishal@gmail.com>
2024-03-18 10:44:19 +0530

    Update requirements and start script

--- src/start.sh
+++ src/start.sh
@@ -2,6 +2,9 @@
 
 echo "Worker Initiated"
 
+echo "Install Dependency"
+pip install -r /workspace/sd/stable-diffusion-webui/requirements_versions.txt
+
 echo "Starting WebUI API"
 python /workspace/sd/stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --lowram --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
 
